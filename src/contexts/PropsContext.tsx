638d38ffb7aab765e65156a0eb24faf103c76736
Martijn Vermeulen <martijn.vermeulen10@gmail.com>
2022-02-07 22:47:14 +0100

    Opened platinum

--- src/contexts/PropsContext.tsx
+++ src/contexts/PropsContext.tsx
@@ -51,7 +51,7 @@ export const PropsProvider = ({ children }: { children: React.ReactNode }) => {
   const [interval] = useState(1000)
 
   const [diff, setDiff] = useState(
-    moment.tz("2021-03-03 20:22", "United States/New York").valueOf() -
+    moment.tz("2022-03-03 20:22", "United States/New York").valueOf() -
       moment().valueOf()
   )
   const [ready, setReady] = useState(Math.floor(diff / 1000) < 0)
