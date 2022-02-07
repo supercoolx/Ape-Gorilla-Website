bf6d48f98ca1dc79cf2c0d87df4541e96b008f45
Martijn Vermeulen <martijn.vermeulen10@gmail.com>
2022-02-07 22:19:42 +0100

    Thumbnail

--- src/utils/video/VideoPlayIcon.tsx
+++ src/utils/video/VideoPlayIcon.tsx
@@ -1,4 +1,4 @@
-import Video from "assets/img/video.jpg"
+import Video from "assets/img/thumbnail.png"
 import React from "react"
 import { MdPlayCircleFilled } from "react-icons/md"
 import Wrapper from "utils/wrapper/Wrapper"
@@ -17,8 +17,8 @@ const VideoPlayIcon = ({
           <img src={Video} alt="" />
         </div>
         <div className="flex w-full items-center justify-center">
-          <div className="relative h-40 w-40 rounded-full shadow-md">
-            <MdPlayCircleFilled className="absolute -top-4 -left-4 text-48 text-white" />
+          <div className="relative h-[54px] w-[54px] rounded-full bg-white shadow-md">
+            <MdPlayCircleFilled className="absolute -top-8 -left-8 text-[72px] text-green-light" />
           </div>
           <Wrapper open={title ? true : false}>
             <div className="absolute top-10 left-10 text-14 font-bold text-white md:top-18 md:left-18">
